1065569b31f28c1ee097c2d66b7a202a650b4899
UpcraftLP <j-schirra@gmx.de>
2019-01-22 09:29:19 +0100

    fix collision detection

--- src/main/java/com/github/glasspane/blink/util/RayHelper.java
+++ src/main/java/com/github/glasspane/blink/util/RayHelper.java
@@ -31,23 +31,28 @@ public class RayHelper {
     public static Vec3d raytrace(Entity entity, float deltaTime) {
         World world = entity.world;
         HitResult trace = MeshHelper.rayTraceEntity(entity, Blink.BLINK_RANGE, RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.SOURCE_ONLY, deltaTime);
-        switch(trace.getType()) {
-            case ENTITY:
-                //TODO back up by entity collision box size
-                break;
-            case NONE:
-                trace = MeshHelper.rayTrace(world, entity, trace.getPos(), trace.getPos().subtract(0, 1, 0), RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.SOURCE_ONLY);
-                break;
+        boolean secondPass;
+        if(trace.getType() == HitResult.Type.NONE) {
+            trace = MeshHelper.rayTrace(world, entity, trace.getPos(), trace.getPos().subtract(0, 1, 0), RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.SOURCE_ONLY);
+            secondPass = false;
+            //secondPass = trace.getType() != HitResult.Type.NONE;
+        }
+        else {
+            secondPass = true;
         }
         Vec3d pos = trace.getPos();
         if(trace.getType() == HitResult.Type.BLOCK) {
             BlockHitResult result = (BlockHitResult) trace;
             switch(result.getSide()) {
-                case UP:
-                    break;
                 case DOWN:
                     pos = pos.subtract(0, entity.getHeight(), 0);
+                    break;
+                case UP:
+                    secondPass = false;
+                    break;
                 default:
+                    Vec3d entityPos = entity.getCameraPosVec(deltaTime);
+                    Vec3d toTarget = pos.subtract(entityPos);
                     if(pos.y - (int) pos.y >= 0.5D) {
                         BlockPos testPos;
                         switch(result.getSide()) {
@@ -60,38 +65,25 @@ public class RayHelper {
                             default:
                                 testPos = new BlockPos(pos.x, pos.y + 1, pos.z);
                         }
-                        Vec3d entityPos = entity.getCameraPosVec(deltaTime);
-                        Vec3d toTarget = pos.subtract(entityPos);
-                        if(world.isAir(testPos) && world.isAir(testPos.up())) {
-                            toTarget.multiply(Math.max((toTarget.length() + 0.8D) / toTarget.length(), 1.0D));
+                        if(world.isAir(testPos) && world.isAir(testPos.up())) { //TODO check all blocks in BB
+                            toTarget = toTarget.multiply(Math.max((toTarget.length() + 0.8D) / toTarget.length(), 1.0D));
                             pos = new Vec3d(entityPos.x + toTarget.x, testPos.getY(), entityPos.z + toTarget.z);
+                            secondPass = false;
                         }
                     }
-//                    //unstuck from wall
-//                    playerToTarget = playerToTarget.multiply(Math.max((playerToTarget.length() - (entity.width / 2.0F) - 0.0001D) / playerToTarget.length(), 0.0001D));
-//                    Vec3d newOrigin = playerPos.add(playerToTarget);
-//                    Vec3d newEnd = newOrigin.add(0.0D, -1.0D, 0.0D);
-//
-//                    result = MeshHelper.rayTraceEntity(entity.world, newOrigin, newEnd, RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.SOURCE_ONLY);
-//                    if(result == null) {
-//                        Vec3d diff = newOrigin.subtract(newEnd);
-//                        result = BlockHitResult.createMissed(newEnd, Direction.getFacing(diff.x, diff.y, diff.z), new BlockPos(newEnd)); //new HitResult(HitResult.Type.NONE, newEnd, null, new BlockPos(newEnd));
-//                    }
-//                    //TODO actually we should raytrace UP from here, then check if length() >= player height
-//                    Vec3d ret;
-//                    loop:
-//                    for(int i = 0; i < 3; i++) {
-//                        ret = result.pos.subtract(0.0D, i, 0.0D);
-//                        for(int j = 0; j <= entity.height + 1; j++) {
-//                            if(!entity.world.isAir(new BlockPos(ret.x, ret.y + j, ret.z))) {
-//                                continue loop;
-//                            }
-//                        }
-//                        return ret;
-//                    }
+                    if(secondPass) {
+                        toTarget = toTarget.multiply((toTarget.length() - (entity.getWidth() * 1.2F)) / toTarget.length()); //half of bounding box width
+                        pos = entityPos.add(toTarget);
+                    }
             }
         }
-        //TODO unstuck entity box here
+        if(secondPass) {
+            Vec3d tempPos = pos.subtract(0.0D, 0.0001D, 0.0D);
+            HitResult flagTrace = MeshHelper.rayTrace(world, entity, tempPos, pos.add(0.0D, entity.getHeight(), 0.0D), RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.SOURCE_ONLY);
+            if(flagTrace.getPos().y - tempPos.y < entity.getHeight()) {
+                pos = flagTrace.getPos().subtract(0, entity.getHeight(), 0);
+            }
+        }
         return pos;
     }
 }
